614d7579ebd35ee1b403e096621dde1deb7f6477
joy <narumoljoy@gmail.com>
2021-01-08 03:12:10 +0700

    Create Task03.py

---
+++ Task03.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Task 3
+# # Write a program that allows user to enter their favourite starter, main course, dessert and drink.
+# # Concatenate these and output a message which says – “Your favourite meal is .........with a glass of....”
+
+# In[1]:
+
+
+starter = str(input("Your favourite starter:"))
+
+
+# In[2]:
+
+
+main = str(input("Your favourite main course:"))
+
+
+# In[3]:
+
+
+dessert = str(input("Your favourite dessert:"))
+
+
+# In[4]:
+
+
+drink = str(input("Your favourite drink:"))
+
+
+# In[11]:
+
+
+print("Your favourite meal is " + starter + ", " + main + ", " + dessert + " " + "with a glass of " + drink + ".")
+
+
+# In[ ]:
+
+
+
+
