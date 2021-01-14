bb2b39e08ea07b5ac37ae1ffcb3a4f139f906ce8
joy <narumoljoy@gmail.com>
2021-01-15 03:32:16 +0700

    Create Week02 Task01.py

---
+++ Week02 Task01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Task1: Write a program that asks the user for an integer number and checks if it is > 10. If it is, it will print “Number is Greater than 10”, else “Number is smaller than 10”.
+
+# In[2]:
+
+
+number = int(input("Enter a number"))
+
+if number > 10:
+    print("Number is Greater than 10")
+else:
+    print("Number is smaller than 10")
+    
+
