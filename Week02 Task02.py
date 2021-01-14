b922c968e63d24f290faf9f5061e0ae4417fc945
joy <narumoljoy@gmail.com>
2021-01-15 03:32:19 +0700

    Create Week02 Task02.py

---
+++ Week02 Task02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Task2:Then write a loop program that ask the user for an integer number and check if it is < 10. If it is < 10 then it keeps adding 1 to the value.
+
+# In[2]:
+
+
+number = int(input("Enter a number"))
+
+while number < 10:
+    print("Number is less than 10")
+    
+    number += 1
+
+print("Number is greater than 10")
+
+
+# In[ ]:
+
+
+
+
