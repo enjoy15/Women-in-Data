00967d2a7f27cde00fbef897d712a159a3642698
joy <narumoljoy@gmail.com>
2021-01-15 03:32:04 +0700

    Create Week02 Home Learning 01.py

---
+++ Week02 Home Learning 01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Create your own Data Flow Diagram (DFD), a subject of your own choice, Example: Fast food order and convert it into code.
+
+# In[ ]:
+
+
+# Data Flow Diagram (DFD)
+
+
+# In[ ]:
+
+
+https://lucid.app/lucidchart/invitations/accept/eea64238-e73e-4b9c-ad11-e0e28f7b7301
+
+
+# In[10]:
+
+
+order = input("What do you want to eat: Chicken, Fries?")
+
+if order == "Chicken":
+    print("Your order is Chicken")
+elif order == "Fry":
+    print("Your order is Fries")
+else:
+    print("Please select either Chicken or Fry") 
+
+
+# In[ ]:
+
+
+
+
